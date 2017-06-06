581cca0aa89adb03e09325731fc25eaad9af777b
Your Name <czaczek19@wp.pl>
2017-06-06 19:35:48 +0200

    Add server json result

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, abort, make_response, request
 import uuid
 import os
+import json
 from pymongo import MongoClient
 from vokaturi.analyzer import analyze_file
 from converter.amr2wav import convert
@@ -10,13 +11,21 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found sth else'}), 404)
 
 
 @app.route('/results', methods=['GET'])
 def get_results_all():
     # todo: get all results
-    abort(404)
+    collection = MongoClient(os.getenv('MONGOLAB_URI'))['depressiondata']['results']
+    result = collection.find()
+    result_list = []
+    for res in result:
+        result_map = {}
+        for single in res:
+            result_map[single] = str(res[single])
+        result_list.append(result_map)
+    return make_response(jsonify(result_list), 200)
 
 
 @app.route('/results/<user_id>', methods=['GET'])
@@ -24,17 +33,20 @@ def get_results_by_user(user_id):
     # todo: get results of one user
     collection = MongoClient(os.getenv('MONGOLAB_URI'))['depressiondata']['results']
     result = collection.find({'user': str(user_id)})
-    mess = ''
+    result_list = []
     for res in result:
-        mess += str(res) + '\n'
-    return make_response(jsonify(message=mess), 200)
+        result_map = {}
+        for single in res:
+            result_map[single] = str(res[single])
+        result_list.append(result_map)
+    return make_response(jsonify(result_list), 200)
 
 
 @app.route('/sound_files', methods=['POST'])
 def post_sound_file():
     filename = uuid.uuid4().hex
-    amr_filename = f'{filename}.amr'
-    wav_filename = f'{filename}.wav'
+    amr_filename = filename + '.amr'
+    wav_filename = filename + '.wav'
     with open(amr_filename, 'wb') as file:
         file.write(request.get_data())
     convert(amr_filename)
@@ -46,7 +58,6 @@ def post_sound_file():
     if emotions:
         return make_response(jsonify({'received': True}), 200)
     return make_response(jsonify({'error': 'Failure while analyzing file'}), 400)
-
 
 
 if __name__ == '__main__':

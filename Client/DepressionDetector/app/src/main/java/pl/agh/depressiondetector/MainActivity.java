05cc4e81b29040245bacc061a6cfa00c814299b2
Michal Baran <michalasakura@gmail.com>
2017-08-20 21:09:30 +0200

    Added authorization to request for results

--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/MainActivity.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/MainActivity.java
@@ -1,20 +1,36 @@
 package pl.agh.depressiondetector;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Credentials;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import pl.agh.depressiondetector.connection.HttpClient;
 import pl.agh.depressiondetector.settings.SettingsActivity;
+import pl.agh.depressiondetector.utils.ToastUtils;
+
+import static pl.agh.depressiondetector.connection.API.HOST;
+import static pl.agh.depressiondetector.connection.API.PATH_RESULTS;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @BindView(R.id.textview_main_results)
     TextView resultsView;
@@ -45,20 +61,61 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_main_getresults)
     public void onGetResultsClick() {
+        // TODO Replace somewhere, add showing all results
         new AsyncTask<Void, Void, String>() {
 
             @Override
             protected String doInBackground(Void... params) {
-                return HttpClient.getInstance().getLatestResult();
+                return getLatestResult();
             }
 
             @Override
-            protected void onPostExecute(String s) {
+            protected void onPostExecute(String result) {
+                Log.i(TAG, "Results are: " + result);
                 if (resultsView != null) {
-                    int i = s.indexOf("happy");
-                    resultsView.setText("You were " + s.substring(i + 9, i + 14) + " happy last time");    // TODO JSON Parsing
+                    if (result.contains("happy")) {
+                        int i = result.indexOf("happy");
+                        resultsView.setText("You were " + result.substring(i + 9, i + 14) + " happy last time");
+                    } else
+                        ToastUtils.show(MainActivity.this, "Empty results!");
                 }
             }
         }.execute();
+    }
+
+    private String getLatestResult() {
+        String result = null;
+        try {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            String login = preferences.getString(getString(R.string.pref_user_username), "");
+            String password = preferences.getString(getString(R.string.pref_user_password), "");
+
+            String credential = Credentials.basic(login, password);
+
+            HttpUrl apiUrl = new HttpUrl.Builder()
+                    .scheme("https")
+                    .host(HOST)
+                    .addEncodedPathSegments(PATH_RESULTS)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(apiUrl)
+                    .header("Authorization", credential)
+                    .get()
+                    .build();
+
+            Response response = HttpClient.getClient().newCall(request).execute();
+            ResponseBody body = response.body();
+            Log.i(TAG, "Server returned: " + response.message() + " with code " + response.code());
+            if (body != null) {
+                if (response.isSuccessful())
+                    result = body.string();
+                body.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/connection/HttpClient.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/connection/HttpClient.java
@@ -1,18 +1,7 @@
 package pl.agh.depressiondetector.connection;
 
-import android.support.annotation.Nullable;
-
-import java.io.IOException;
-
-import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
-
-import static pl.agh.depressiondetector.connection.API.HOST;
-import static pl.agh.depressiondetector.connection.API.PATH_RESULTS;
 
 public class HttpClient {
 
@@ -26,44 +15,9 @@ public class HttpClient {
         client = new OkHttpClient();
     }
 
-    public static HttpClient getInstance() {
-        if (instance == null)
-            instance = new HttpClient();
-        return instance;
-    }
-
     public static OkHttpClient getClient() {
         if (instance == null)
             instance = new HttpClient();
         return client;
-    }
-
-    @Nullable
-    public String getLatestResult() {
-        String result = null;
-        try {
-            HttpUrl apiUrl = new HttpUrl.Builder()
-                    .scheme("https")
-                    .host(HOST)
-                    .addEncodedPathSegments(PATH_RESULTS)
-                    .build();
-
-            Request request = new Request.Builder()
-                    .url(apiUrl)
-                    .get()
-                    .build();
-
-            Response response = client.newCall(request).execute();
-            ResponseBody body = response.body();
-            if (body != null) {
-                if (response.isSuccessful())
-                    result = body.string();
-                body.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return result;
     }
 }

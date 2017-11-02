57587ebfb11ed2b8022a7304e949ebc6111de25c
PPitson <pitu5599@gmail.com>
2017-11-02 22:03:31 +0100

    Added tests for /text_results endpoint

--- Server/tests/test_main_blueprint.py
+++ Server/tests/test_main_blueprint.py
@@ -24,6 +24,24 @@ class VoiceEmotionResultsTestCase(CustomTestCase):
             self.assertIn('date', result)
             self.assertNotIn('text_happiness_level', result)
 
+    def test_unauthorized_get_text_results(self):
+        response = self.client.get('/text_results')
+        self.assert401(response)
+
+    def test_get_text_results(self):
+        now = datetime.utcnow().date()
+        for i in range(3):
+            HappinessLevel.objects.create(user=self.user, date=now - timedelta(days=i),
+                                          voice_happiness_level=0.1 * i, text_happiness_level=0.1 * i)
+
+        response = self.client.get('/text_results', headers=self.get_headers())
+        self.assert200(response)
+        self.assertEqual(len(response.json), 3)
+        for result in response.json:
+            self.assertIn('text_happiness_level', result)
+            self.assertIn('date', result)
+            self.assertNotIn('voice_happiness_level', result)
+
     def test_unauthorized_post_sound_file(self):
         response = self.client.post('/sound_files', data=b'123')
         self.assert401(response)

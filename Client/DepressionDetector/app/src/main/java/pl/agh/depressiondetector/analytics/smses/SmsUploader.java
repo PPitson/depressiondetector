b4929447cb0310e3947c1ac59e32c01538704cd5
jsamol <julia.samol@gmail.com>
2017-11-06 17:17:57 +0100

    Implement upload method and add new support private methods

--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/analytics/smses/SmsUploader.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/analytics/smses/SmsUploader.java
@@ -2,13 +2,57 @@ package pl.agh.depressiondetector.analytics.smses;
 
 import android.content.Context;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import pl.agh.depressiondetector.scheduler.Uploader;
+
+import static pl.agh.depressiondetector.connection.API.PATH_TEXT_MESSAGES;
+import static pl.agh.depressiondetector.utils.FileUtils.getTextMessageFileName;
+import static pl.agh.depressiondetector.utils.FileUtils.getTextMessagesDirectory;
+import static pl.agh.depressiondetector.utils.NetworkUtils.postJSONArray;
+
 
 
 public class SmsUploader implements Uploader {
     @Override
     public boolean upload(Context appContext) {
-        // TODO Implement
-        return true;
+        boolean success = false;
+        File directory = getTextMessagesDirectory();
+        File file = new File(directory, getTextMessageFileName() + ".txt");
+        try {
+            if (postTxtFile(file, appContext))
+                success = file.delete();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+
+    private boolean postTxtFile(File file, Context context) throws IOException, JSONException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        String jsonArrayString = readFileInputStream(fileInputStream);
+        fileInputStream.close();
+        JSONArray jsonArray = new JSONArray(jsonArrayString);
+        return postJSONArray(jsonArray, context, PATH_TEXT_MESSAGES);
+    }
+
+    private String readFileInputStream(FileInputStream fileInputStream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+
+        String line = bufferedReader.readLine();
+
+        stringBuilder.append(line);
+        stringBuilder.append("]");
+
+        bufferedReader.close();
+        return stringBuilder.toString();
     }
 }

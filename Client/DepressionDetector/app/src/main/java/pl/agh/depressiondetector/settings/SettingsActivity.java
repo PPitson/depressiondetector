3d21a7f7462fe7b679c4def963f885f760f47a8d
Michal Baran <michalasakura@gmail.com>
2017-11-18 14:58:56 +0100

    Added asking for permissions in SettingsActivity

--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/settings/SettingsActivity.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/settings/SettingsActivity.java
@@ -1,12 +1,26 @@
 package pl.agh.depressiondetector.settings;
 
+import android.Manifest;
 import android.content.SharedPreferences;
 
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.BasePermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.agh.depressiondetector.R;
+import pl.agh.depressiondetector.utils.ToastUtils;
 
 import static pl.agh.depressiondetector.analytics.AnalysedDataType.MOOD;
 import static pl.agh.depressiondetector.analytics.AnalysedDataType.PHONE_CALL;
@@ -16,15 +30,19 @@ import static pl.agh.depressiondetector.analytics.AnalyticsAdapter.stopAnalytics
 
 public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    private SettingsFragment settingsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_settings);
 
+        settingsFragment = new SettingsFragment();
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.framelayout_settings, new SettingsFragment())
+                .replace(R.id.framelayout_settings, settingsFragment)
                 .commit();
     }
 
@@ -36,35 +54,111 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
     public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
         boolean newValue = preferences.getBoolean(key, false);
 
         if (key.equals(getString(R.string.pref_analyse_mood))) {
             if (newValue)
-                startAnalytics(this, MOOD);
+                askForMoodPermissionsAndStartAnalytics();
             else
                 stopAnalytics(this, MOOD);
         }
 
         if (key.equals(getString(R.string.pref_analyse_phone_calls))) {
             if (newValue)
-                startAnalytics(this, PHONE_CALL);
+                askForPhoneCallsPermissionsAndStartAnalytics();
             else
                 stopAnalytics(this, PHONE_CALL);
         }
 
         if (key.equals(getString(R.string.pref_analyse_text_messages))) {
             if (newValue)
-                startAnalytics(this, SMS);
+                askForTextMessagesPermissionsAndStartAnalytics();
             else
                 stopAnalytics(this, SMS);
         }
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(this);
+
+    private void askForMoodPermissionsAndStartAnalytics() {
+        BasePermissionListener listener = new BasePermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                startAnalytics(SettingsActivity.this, MOOD);
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                settingsFragment.setMoodPreferenceChecked(false);
+                ToastUtils.show(SettingsActivity.this, "We can't start analytics without this permission");
+            }
+        };
+
+        askForPermission(listener, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    private void askForPhoneCallsPermissionsAndStartAnalytics() {
+        BaseMultiplePermissionsListener listener = new BaseMultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()) {
+                    startAnalytics(SettingsActivity.this, PHONE_CALL);
+                } else {
+                    settingsFragment.setPhoneCallsPreferenceChecked(false);
+                    ToastUtils.show(SettingsActivity.this, "We can't start analytics without this permission");
+                }
+            }
+        };
+
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_PHONE_STATE);
+        permissions.add(Manifest.permission.PROCESS_OUTGOING_CALLS);
+        permissions.add(Manifest.permission.RECORD_AUDIO);
+        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        askForPermissions(listener, permissions);
+    }
+
+    private void askForTextMessagesPermissionsAndStartAnalytics() {
+        BaseMultiplePermissionsListener listener = new BaseMultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                if (report.areAllPermissionsGranted()) {
+                    startAnalytics(SettingsActivity.this, SMS);
+                } else {
+                    settingsFragment.setTextMessagesPreferenceChecked(false);
+                    ToastUtils.show(SettingsActivity.this, "We can't start analytics without this permission");
+                }
+            }
+        };
+
+        List<String> permissions = new ArrayList<>(3);
+        permissions.add(Manifest.permission.READ_SMS);
+        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        askForPermissions(listener, permissions);
+    }
+
+    private void askForPermission(PermissionListener listener, String permission) {
+        Dexter.withActivity(this)
+                .withPermission(permission)
+                .withListener(listener)
+                .onSameThread()
+                .check();
+    }
+
+    private void askForPermissions(MultiplePermissionsListener listener, List<String> permissions) {
+        Dexter.withActivity(this)
+                .withPermissions(permissions)
+                .withListener(listener)
+                .onSameThread()
+                .check();
     }
 }
--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/settings/SettingsFragment.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/settings/SettingsFragment.java
@@ -1,15 +1,48 @@
 package pl.agh.depressiondetector.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SwitchPreferenceCompat;
 
 import pl.agh.depressiondetector.R;
+
+import static android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences;
+import static pl.agh.depressiondetector.analytics.AnalysedDataType.MOOD;
+import static pl.agh.depressiondetector.analytics.AnalysedDataType.PHONE_CALL;
+import static pl.agh.depressiondetector.analytics.AnalysedDataType.SMS;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    private SwitchPreferenceCompat moodPreference;
+    private SwitchPreferenceCompat phoneCallsPreference;
+    private SwitchPreferenceCompat textMessagesPreference;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        moodPreference = (SwitchPreferenceCompat) findPreference(MOOD.preferenceName);
+        phoneCallsPreference = (SwitchPreferenceCompat) findPreference(PHONE_CALL.preferenceName);
+        textMessagesPreference = (SwitchPreferenceCompat) findPreference(getString(R.string.pref_analyse_text_messages));
+
+        SharedPreferences preferences = getDefaultSharedPreferences(getContext());
+        setMoodPreferenceChecked(preferences.getBoolean(MOOD.preferenceName, false));
+        setPhoneCallsPreferenceChecked(preferences.getBoolean(PHONE_CALL.preferenceName, false));
+        setTextMessagesPreferenceChecked(preferences.getBoolean(SMS.preferenceName, false));
+    }
+
+    public void setMoodPreferenceChecked(boolean value) {
+        moodPreference.setChecked(value);
+    }
+
+    public void setPhoneCallsPreferenceChecked(boolean value) {
+        phoneCallsPreference.setChecked(value);
+    }
+
+    public void setTextMessagesPreferenceChecked(boolean value) {
+        textMessagesPreference.setChecked(value);
     }
 }

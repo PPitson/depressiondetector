d5818f248d6588f7b65ed08336d7bc763d717678
jsamol <julia.samol@gmail.com>
2017-12-06 21:45:19 +0100

    Check if the list index is not out of boundires and format date depending on the chart type

--- Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/utils/results/plot/AxisValueFormatter.java
+++ Client/DepressionDetector/app/src/main/java/pl/agh/depressiondetector/utils/results/plot/AxisValueFormatter.java
@@ -3,15 +3,22 @@ package pl.agh.depressiondetector.utils.results.plot;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import pl.agh.depressiondetector.utils.DateUtils;
 
 
 public class AxisValueFormatter implements IAxisValueFormatter {
-    List<String> dates;
+    private List<String> dates;
+    private String format;
 
-    AxisValueFormatter() {
+    AxisValueFormatter(String format) {
         this.dates = new ArrayList<>();
+        this.format = format;
     }
 
     void addDate(String date) {
@@ -20,6 +27,11 @@ public class AxisValueFormatter implements IAxisValueFormatter {
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
-        return dates.get((int) value);
+        if (0 <= (int) value && (int) value < dates.size()) {
+            String dateString = dates.get((int) value);
+            return new SimpleDateFormat(format, Locale.getDefault())
+                    .format(DateUtils.getDateFromClientDateFormat(dateString));
+        }
+        return "";
     }
 }
